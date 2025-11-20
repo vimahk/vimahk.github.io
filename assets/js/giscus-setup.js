94d67304d6f113b83db85aef9b63b3d561208fe5
vimahk <kebritam@proton.me>
2025-11-20 18:03:31 +0000

    Deploying to gh-pages from @ vimahk/vimahk.github.io@b6f79ac204b1772ec7013589142d2f05292acabd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vimahk/vimahk.github.io-al-folio",
+    "data-repo": "vimahk/vimahk.github.io",
     "data-repo-id": "R_kgDOQHYVDA",
     "data-category": "Comments",
     "data-category-id": "",
