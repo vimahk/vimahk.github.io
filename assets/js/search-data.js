40adb8cb660a1eab8b7af294fb9c738816e7c8f0
vimahk <kebritam@proton.me>
2025-11-20 17:31:11 +0000

    Deploying to gh-pages from @ vimahk/vimahk.github.io-al-folio@8330ce91c3647d2c11e552da93bb8f6f1d8d22b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-a-post-with-code",
+        
+          title: "a post with code",
+        
+        description: "an example of a blog post with some code",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/code/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
